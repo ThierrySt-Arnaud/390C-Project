b149196f300ab730328c6d575962db15e0e3dfa5
thilakse <thilakse@gmail.com>
2018-03-19 17:16:32 -0400

    - Created addData method

--- SoundLevelApp/app/src/main/java/ca/concordia/teamc/soundlevelapp/DatatabaseHelper.java
+++ SoundLevelApp/app/src/main/java/ca/concordia/teamc/soundlevelapp/DatatabaseHelper.java
@@ -1,5 +1,6 @@
 package ca.concordia.teamc.soundlevelapp;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -15,13 +16,13 @@ public class DatatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "SoundMeterTable.db";
     private static final String TABLE_NAME = "SoundMeterTable";
     private static final String COL0 = "ID";
-    private static final String COL1 = "sensor_name";
-    private static final String COL2 = "MAC_Adress";
-    private static final String COL3 = "Location";
-    private static final String COL4 = "LastKnownProject";
-    private static final String COL5 = "LastConnectionDate";
-    private static final String COL6 = "RecordingStatus";
-    private static final String COL7 = "StartRecordingDate";
+    private static final String COL1 = "SENSOR_NAME";
+    private static final String COL2 = "MAC_ADDRESS";
+    private static final String COL3 = "LOCATION";
+    private static final String COL4 = "LAST_KNOWN_PROJECT";
+    private static final String COL5 = "LAST_CONNECTION_DATE";
+    private static final String COL6 = "RECORDING_STATUS";
+    private static final String COL7 = "START_RECORDING_DATA";
 
 
 
@@ -30,13 +31,35 @@ public class DatatabaseHelper extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String createTable = "CREATE TABLE " + TABLE_NAME + "ID INTEGER PRIMARY KEY AUTOINCREMENT, "+ COL1 +" TEXT";
+        String createTable = "CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                " SENSOR_NAME TEXT, MAC_ADDRESS TEXT, LOCATION TEXT, LAST_KNOWN_PROJECT TEXT, LAST_CONNECTION_DATE TEXT, RECORDING_STATUS TEXT, START_RECORDING_DATA TEXT)";
         sqLiteDatabase.execSQL(createTable);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP IF TABLE EXISTS "+ TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
 
+    public boolean addData(String sensorName,String macAddress, String location, String lastKnownProject, String lastConnectionDate , String recordingStatus, String startRecordingData){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL1, sensorName);
+        contentValues.put(COL2, macAddress);
+        contentValues.put(COL3, location);
+        contentValues.put(COL4, lastKnownProject);
+        contentValues.put(COL5, lastConnectionDate);
+        contentValues.put(COL6, recordingStatus);
+        contentValues.put(COL7, startRecordingData);
+
+        long result = sqLiteDatabase.insert(TABLE_NAME, null, contentValues);
+
+        if(result == -1){
+            return false;
+        }else{
+            return true;
+        }
     }
 }
 
