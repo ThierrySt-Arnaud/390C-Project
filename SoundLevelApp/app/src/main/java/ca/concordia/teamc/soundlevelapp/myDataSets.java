de4960a0da007df536e8660f08cbca7e45315dd1
Sharmilan S <sharmilan.s@hotmail.com>
2018-03-23 17:26:07 -0400

    I have added the following items: -Updated the Datasets List as baseAdapter configurations. -Data class to have the baseadapter class. -Removed the "ifRoom" option on the actionmenu.

--- SoundLevelApp/app/src/main/java/ca/concordia/teamc/soundlevelapp/myDataSets.java
+++ SoundLevelApp/app/src/main/java/ca/concordia/teamc/soundlevelapp/myDataSets.java
@@ -2,6 +2,9 @@ package ca.concordia.teamc.soundlevelapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.view.Menu;
@@ -12,21 +15,46 @@ import android.content.Intent;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.content.ContentValues.TAG;
+
 public class myDataSets extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mydatasets);
-        List<String> Mylist = new ArrayList<>();
+       /* List<String> Mylist = new ArrayList<>();
         Mylist.add("For Sprint 2");
 
         ListView listView = (ListView) findViewById(R.id.listview);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,Mylist);
-        listView.setAdapter(adapter);
+        listView.setAdapter(adapter);*/
 
+        List<Data> Mylist = new ArrayList<>();
+        Data dataA = new Data("Project A","Location","Date Started","Date Downloaded" );
+        Data dataB = new Data("Project B","Location","Date Started","Date Downloaded");
+        Data dataC = new Data("Project C","Location","Date Started","Date Downloaded");
 
+        Mylist.add(dataA);
+        Mylist.add(dataB);
+        Mylist.add(dataC);
+
+        DataListAdapter dataListAdapter = new DataListAdapter(this, Mylist);
+        ListView listView = (ListView) findViewById(R.id.listview);
+
+        listView.setAdapter(dataListAdapter);
+        listView.setOnItemClickListener(dataListListener);
     }
+
+    AdapterView.OnItemClickListener dataListListener = new AdapterView.OnItemClickListener() {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Log.d(TAG, "Item " + i + "  got clicked");
+            Intent myIntent = new Intent(adapterView.getContext(), myDataSets_Detailed.class);
+            startActivity(myIntent);
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
