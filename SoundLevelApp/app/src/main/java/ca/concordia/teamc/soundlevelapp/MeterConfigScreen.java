c8a0bc095e6b3162dffbeab926a872575b77aff4
DinaAlyousef <36862195+DinaAlyousef@users.noreply.github.com>
2018-03-23 08:55:06 -0400

    Bluetooth Service & msg handeling in MeterConfigScreen
    WIP

--- SoundLevelApp/app/src/main/java/ca/concordia/teamc/soundlevelapp/MeterConfigScreen.java
+++ SoundLevelApp/app/src/main/java/ca/concordia/teamc/soundlevelapp/MeterConfigScreen.java
@@ -1,7 +1,14 @@
 package ca.concordia.teamc.soundlevelapp;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,6 +23,10 @@ public class MeterConfigScreen extends AppCompatActivity{
     protected EditText LocationText =null;
     protected Button saveButton = null;
     Profile profile = new Profile();
+    BroadcastReceiver mReceiver;
+    BluetoothService BTService;
+    boolean bound=false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +59,82 @@ public class MeterConfigScreen extends AppCompatActivity{
                     profile.setLocation(LocationText.getText().toString());
 
                     sharedPreferenceHelper.saveProfileName(profile);
+
+                    BTService.write("project: "+ ProjectText.getText().toString());
+                    BTService.write("location: "+ LocationText.getText().toString());
+
                     editText(false);
                     Toast toast = Toast.makeText(getApplicationContext(), "Saved!", Toast.LENGTH_LONG);
                     toast.show();
                 }
             }
         });
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothService.BT_MESSAGE);
+
+        mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String msg = intent.getStringExtra("message");
+                Log.d("Receiver", "got message: "+ msg);
+                if (msg.lastIndexOf((char) 0x1A) != -1){
+                    Log.d("DETECTED", "detected EOF");
+                }
+                if (msg.lastIndexOf((char) 0x0A) != -1 || msg.lastIndexOf((char) 0x0D) != -1){
+                    Log.d("DETECTED", "detected NL");
+                }
+
+                String[] file = msg.split("<<<");
+                String[] config = file[0].split("\\r?\\n"); // doesn't work
+                String[] data = file[1].replace(">>>","").split("\\r?\\n"); // doesn't work
+
+                for(String str : config){
+                    Log.d("CONFIG", str);
+                }
+
+                for(String str : data){
+                    Log.d("DATA", str);
+                }
+
+                ProjectText.setText(config[0]);
+                LocationText.setText(config[1]);
+
+            }
+        };
+
+        registerReceiver(mReceiver, filter);
     }
+
+    public void onStart(){
+        super.onStart();
+        Intent intent = getIntent();
+        String project = intent.getStringExtra("projectName");
+        String location = intent.getStringExtra("meterLocation");
+
+        ProjectText.setText(project);
+        LocationText.setText(location);
+
+        Intent BTSIntent = new Intent(this, BluetoothService.class);
+        bindService(BTSIntent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            BluetoothService.LocalBinder binder = (BluetoothService.LocalBinder) service;
+            BTService = binder.getService();
+            bound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            bound = false;
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,7 +150,6 @@ public class MeterConfigScreen extends AppCompatActivity{
         }
 
         if (item.getItemId() == R.id.action_datasets) {
-
 
             Intent intent= new Intent(this, myDataSets.class);
             startActivity(intent);
@@ -99,6 +178,16 @@ public class MeterConfigScreen extends AppCompatActivity{
         LocationText.setFocusableInTouchMode(bool);
         LocationText.setFocusable(bool);
         //editText(bool: true);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mReceiver != null) {
+            unregisterReceiver(mReceiver);
+            mReceiver = null;
+        }
+
+        super.onDestroy();
     }
 }
 
